a02c5c6541f5548ba7a0088c8a2db94bc6dc86aa
xenakisin58 <david_wickland@hotmail.com>
2017-09-27 12:38:08 -0400

    Create Birthday_Chocolate.java

---
+++ Birthday_Chocolate.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int solve(int n, int[] s, int d, int m){
+        // Complete this function
+        int result = 0;
+        for(int i=0;i<s.length - m + 1; i++){
+            int temp_sum=0;
+            for(int j=0;j<m;j++){
+                temp_sum+=s[j+i];
+            }
+            if(temp_sum==d){
+                result++;
+            } 
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int d = in.nextInt();
+        int m = in.nextInt();
+        int result = solve(n, s, d, m);
+        System.out.println(result);
+    }
+}
