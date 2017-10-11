cbf551041f91a3289a1193556bc9150a41003234
xenakisin58 <david_wickland@hotmail.com>
2017-10-10 23:02:06 -0400

    Create Intro_to_Tutorial_Challenges.java

---
+++ Intro_to_Tutorial_Challenges.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i] == v){
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+}
